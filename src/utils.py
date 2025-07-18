c40a4887795df926b350061e2e8d83c9d93ac66d
adithya-santhosh <119097200+adithya-santhosh@users.noreply.github.com>
2025-07-18 23:36:09 +0530

    HyperParameter Tunning

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,9 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
-
+from src.logger import logging
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -17,14 +18,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models,param):
     try:
         report = {}
         for i in range(len(list(models))):
-            model = list(models.values())[i]
-
-            model.fit(X_train, y_train)
             
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
